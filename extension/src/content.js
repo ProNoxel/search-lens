c58c1ea42ebf341f77959dfd6775fc48efcff645
ProNoxel <pronoxel@gmail.com>
2026-07-30 14:01:48 +0400

    WIP: prototype advertisement detection

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,1 +1,11 @@
 console.log("Search Lens active");
+
+const detectedAds = detectAds();
+
+console.log("[Search Lens] Detection result:", detectedAds);
+
+if (detectAds.lenght > 0) {
+    console.log("[Search Lens] First detected element:", detectedAds[0].element);
+} else {
+    console.log("[Search Lens] No advertisements detected.");
+}
--- extension/src/detectors/adDetector.js
+++ extension/src/detectors/adDetector.js
@@ -1,5 +1,67 @@
-export function detectAds (root = document) {
-     console.log ("[Seacrh Lens] Starting advertisement detection...");
+function isVisible(element) {
+    const style = window.getComputedStyle(element);
 
-     return [];
+    return (
+        style.display !== "none" &&
+        style.visibility !== "hidden"
+    );
+}
+
+function createDetection(element, reason, confidence) {
+    return {
+        element,
+        type: "advertisement",
+        reason,
+        confidence
+    };
+}
+
+function detectAds(root = document) {
+     console.log ("[Seacrh Lens] Starting advertisement detection...");
+    
+     const detectedAds = [];
+
+     const ignoredTags = [
+    "SCRIPT",
+    "STYLE",
+    "NOSCRIPT",
+    "NOFRAMES"
+];
+
+const elements = root.querySelectorAll("*");
+
+     elements.forEach(element => {
+
+        if (ignoredTags.includes(element.tagName)) {
+    return;
+}
+
+const text = element.innerText;
+
+if (text && text.includes("Рeклама")) {
+    console.log(
+        "[Search Lens] Candidate:",
+        element.tagName,
+        element,
+        isVisible(element)
+    );
+}
+
+if (
+    text && 
+    text.includes("Реклама") &&
+    isVisible(element)
+) {
+    detectedAds.push(
+                createDetection(
+                    element,
+                    ["Contains advertisement label"],
+                    0.9
+                )
+            );
+        }
+
+     });
+
+     return detectedAds;
 }
